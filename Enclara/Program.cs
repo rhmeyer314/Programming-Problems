c02817b1cbd0f15ae475b37ccb799578ddebd776
rhmeyer314 <rhmeyer314@gmail.com>
2020-07-24 07:50:55 -0400

    initial commit

---
+++ Enclara/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Enclara
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
